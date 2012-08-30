49f24ed5b22fff70bbf0a13772da20186afa9178
buerkle <rjefts@gmail.com>
2012-08-29 23:05:21 -0500

    Add initial player model.

---
+++ src/main/java/com/thebuerkle/mcclient/model/Player.java
@@ -0,0 +1,46 @@
+package com.thebuerkle.mcclient.model;
+
+public class Player {
+
+    public static final double HEIGHT = 1.62;
+
+    private Vec3 _position;
+    private Vec3 _velocity = new Vec3();
+    private boolean _onGround;
+    private float _stance;
+
+    public void setOnGround(boolean v) {
+        _onGround = v;
+    }
+
+    public boolean isOnGround() {
+        return _onGround;
+    }
+
+    public Vec3 getPosition() {
+        return _position;
+    }
+
+    public Vec3 getVelocity() {
+        return _velocity;
+    }
+
+    public double getVelocityY() {
+        return _velocity.y;
+    }
+
+    public void setVelocityY(double y) {
+        _velocity.y = y;
+    }
+
+    public void setPosition(Vec3 position) {
+        _position = position;
+    }
+
+    public void setPosition(double x, double y, double z) {
+        _position.x = x;
+        _position.y = y;
+        _position.z = z;
+    }
+}
+
