0599d9616fa537f0afc06c7ba70ca32374a041cc
buerkle <rjefts@gmail.com>
2012-09-01 00:04:22 -0500

    Switch to bouncycastle

--- src/main/java/com/thebuerkle/mcclient/EncryptionFilter.java
+++ src/main/java/com/thebuerkle/mcclient/EncryptionFilter.java
@@ -1,12 +1,11 @@
 package com.thebuerkle.mcclient;
 
+import java.nio.Buffer;
 import java.nio.ByteBuffer;
 import java.security.GeneralSecurityException;
+import java.security.Key;
+import java.security.Security;
 import java.util.Arrays;
-
-import javax.crypto.Cipher;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.IvParameterSpec;
 
 import org.apache.mina.core.buffer.IoBuffer;
 import org.apache.mina.core.filterchain.IoFilter.NextFilter;
@@ -14,62 +13,56 @@ import org.apache.mina.core.filterchain.IoFilterAdapter;
 import org.apache.mina.core.session.IoSession;
 import org.apache.mina.core.write.WriteRequest;
 import org.apache.mina.filter.util.WriteRequestFilter;
+import org.bouncycastle.crypto.BufferedBlockCipher;
+import org.bouncycastle.crypto.CipherKeyGenerator;
+import org.bouncycastle.crypto.KeyGenerationParameters;
+import org.bouncycastle.crypto.engines.AESFastEngine;
+import org.bouncycastle.crypto.io.CipherInputStream;
+import org.bouncycastle.crypto.io.CipherOutputStream;
+import org.bouncycastle.crypto.modes.CFBBlockCipher;
+import org.bouncycastle.crypto.params.KeyParameter;
+import org.bouncycastle.crypto.params.ParametersWithIV;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 public class EncryptionFilter extends WriteRequestFilter {
 
-    private final Cipher _encrypt;
-    private final Cipher _decrypt;
+    private final BufferedBlockCipher _encrypt;
+    private final BufferedBlockCipher _decrypt;
 
-    public EncryptionFilter(SecretKey secret) {
+    static {
+        Security.addProvider(new BouncyCastleProvider());
+    }
+
+    public EncryptionFilter(Key secret) {
         try {
-            _encrypt = Cipher.getInstance("AES/CFB8/NoPadding");
-            _encrypt.init(Cipher.ENCRYPT_MODE, secret, new IvParameterSpec(secret.getEncoded()));
-
-            _decrypt = Cipher.getInstance("AES/CFB8/NoPadding");
-            _decrypt.init(Cipher.DECRYPT_MODE, secret, new IvParameterSpec(secret.getEncoded()));
+            _encrypt = cipher(true, secret);
+            _decrypt = cipher(false, secret);
         }
         catch (Exception e) {
             throw new Error(e);
         }
     }
 
+    private BufferedBlockCipher cipher(boolean encrypt, Key key) {
+        BufferedBlockCipher cipher = new BufferedBlockCipher(new CFBBlockCipher(new AESFastEngine(), 8));
+        cipher.init(encrypt, new ParametersWithIV(new KeyParameter(key.getEncoded()), key.getEncoded(), 0, 16));
+        return cipher;
+    }
+
     @Override()
     public void messageReceived(NextFilter nextFilter, IoSession session, Object message) throws Exception {
         if (message instanceof IoBuffer) {
-/*          byte[] enc = new byte[((IoBuffer) message).remaining()];*/
-/*          ((IoBuffer) message).get(enc);                          */
-/*          ((IoBuffer) message).flip();                            */
-/*          System.err.println("ENCRYPTED: " + toHex(enc));*/
-/*          IoBuffer b = transform(_decrypt, (IoBuffer) message);*/
-
-/*          System.err.println("DECRYPTED: " + b);*/
-/*          byte[] bytes = new byte[b.remaining()];*/
-/*          b.get(bytes);                          */
-
-/*          System.err.println("DECRYPTED: " + toHex(bytes));*/
-/*          b.flip();                              */
-/*          nextFilter.messageReceived(session, b);*/
-            nextFilter.messageReceived(session, transform(_decrypt, (IoBuffer) message));
+            IoBuffer in = (IoBuffer) message;
+            if (in.remaining() > 0) {
+                nextFilter.messageReceived(session, transform(_decrypt, in));
+            }
+            else {
+                System.err.println("--------------- ZERO");
+            }
         }
-        nextFilter.messageReceived(session, message);
-    }
-
-    private String toHex(byte[] b) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < b.length; i++) {
-            sb.append(hex(b[i])).append(' ');
+        else {
+            nextFilter.messageReceived(session, message);
         }
-        return sb.toString();
-    }
-
-    private static String hex(byte b) {
-        String h = "0123456789ABCDEF";
-
-        StringBuilder sb = new StringBuilder();
-        sb.append(h.charAt((b >> 4) & 0x0F));
-        sb.append(h.charAt(b & 0x0F));
-
-        return  sb.toString();
     }
 
     @Override()
@@ -82,21 +75,18 @@ public class EncryptionFilter extends WriteRequestFilter {
         return null; // ignore empty buffers
     }
 
-    private IoBuffer transform(Cipher cipher, IoBuffer in) throws Exception { //GeneralSecurityException {
-        int size = cipher.getOutputSize(in.remaining()+2);
-        int r = in.remaining();
-        IoBuffer result = IoBuffer.allocate(size);
-        try {
-            cipher.update(in.buf(), result.buf());
-            result.flip();
-            return result;
-        }
-        catch (Exception e) {
-            System.err.println("---------- Input: " + size
-                               + " remaining: " + r
-                               + ": " + in.position() + ": " + in.limit());
-            System.err.println("---------- Output: " + result.position() + ": " + result.limit());
-            throw e;
-        }
+    private IoBuffer transform(BufferedBlockCipher cipher, IoBuffer in) throws Exception { //GeneralSecurityException {
+        int size = cipher.getOutputSize(in.remaining());
+        byte[] result = new byte[size];
+        byte[] bytes = in.array();
+
+        cipher.processBytes(bytes, in.position(), in.remaining(), result, 0);
+        return IoBuffer.wrap(result);
+    }
+
+    public void exceptionCaught(NextFilter nextFilter, IoSession session,
+                                Throwable cause) throws Exception {
+        cause.printStackTrace();
+        nextFilter.exceptionCaught(session, cause);
     }
 }
