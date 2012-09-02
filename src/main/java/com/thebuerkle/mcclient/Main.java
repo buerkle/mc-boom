c39fa543a18c83114210c62fd64ba5bab886840e
buerkle <rjefts@gmail.com>
2012-09-02 17:11:25 -0500

    Use a separate thread to reconnect clients

--- src/main/java/com/thebuerkle/mcclient/Main.java
+++ src/main/java/com/thebuerkle/mcclient/Main.java
@@ -4,10 +4,13 @@ import com.thebuerkle.mcclient.request.RequestProtocolEncoder;
 import com.thebuerkle.mcclient.response.ResponseProtocolDecoder;
 
 import java.net.InetSocketAddress;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ScheduledExecutorService;
 
 import org.apache.mina.core.future.ConnectFuture;
+import org.apache.mina.core.polling.AbstractPollingIoConnector;
 import org.apache.mina.core.service.IoConnector;
 import org.apache.mina.core.service.IoHandlerAdapter;
 import org.apache.mina.core.session.IoSession;
@@ -17,42 +20,76 @@ import org.apache.mina.transport.socket.nio.NioSocketConnector;
 
 public class Main {
 
+    private static final String HOST = "localhost";
+    private static final int PORT = 25565;
+
     public static void main(String[] args) {
         new Main().run();
     }
 
     public void run() {
-        final int port = 25565;
-        final String host = "localhost";
-
         final NioSocketConnector connector = new NioSocketConnector();
         SocketSessionConfig cfg = connector.getSessionConfig();
         cfg.setTrafficClass(24); // IPTOS_THROUGHPUT & IPTOS_LOWDELAY
 
         final ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
 
-        ClientListener listener = new ClientListener() {
-            @Override()
-            public void onConnect(Client client) {
+        ReconnectWorker reconnector = new ReconnectWorker(connector, executor);
+        reconnector.start();
 
-            }
-
-            @Override()
-            public void onDisconnect(Client client) {
-                System.err.println("Client disconnected: " + client.getUser() + ". Reconnect");
-                Client reconnect = new Client(executor, client.getUser(), host, port);
-                reconnect.connect(connector);
-            }
-        };
-        connector.setHandler(new ClientHandler(listener));
+        connector.setHandler(new ClientHandler(reconnector));
 
         connector.getFilterChain().addLast("RequestProtocolCodec",
                                            new ProtocolCodecFilter(new RequestProtocolEncoder(), new ResponseProtocolDecoder()));
 
         for (int i = 0; i < 50; i++) {
-            Client client = new Client(executor, "__steve" + i, host, port);
+            Client client = new Client(executor, "__steve" + i, HOST, PORT);
             client.connect(connector);
         }
 /*      connector.dispose();*/
+    }
+
+    private static class ReconnectWorker implements ClientListener, Runnable {
+
+        private final IoConnector _connector;
+        private final ScheduledExecutorService _executor;
+        private final BlockingQueue<Client> _reconnects = new LinkedBlockingQueue<Client>();
+
+        ReconnectWorker(IoConnector connector, ScheduledExecutorService executor) {
+            _connector = connector;
+            _executor = executor;
+        }
+
+        @Override()
+        public void onConnect(Client client) {
+
+        }
+
+        @Override()
+        public void onDisconnect(Client client) {
+            System.err.println("Client disconnected: " + client.getUser());
+            _reconnects.offer(client);
+        }
+
+        public void start() {
+            new Thread(this, "ReconnectWorker").start();
+        }
+
+        @Override()
+        public void run() {
+            Client client = null;
+
+            try {
+                while ((client = _reconnects.take()) != null) {
+                    System.err.println("Reconnect: " + client.getUser());
+
+                    Client reconnect = new Client(_executor, client.getUser(), HOST, PORT);
+                    reconnect.connect(_connector);
+                }
+            }
+            catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
     }
 }
